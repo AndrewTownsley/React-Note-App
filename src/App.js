c7280ab5820b773821170375ea3e9503278920e0
Andrew TOwnsley <atownsley86@gmail.com>
2021-08-06 21:22:03 -0600

    finally got the text from the textarea to render

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import "./App.css";
 
 function App() {
   const [notesArray, setNotesArray] = useState([]);
-  const [noteText, setNoteText] = useState("");
+  const [noteText, setNoteText] = useState('');
 
-// Write a function that creates a new Note Object, and adds the new Note Object to the notesArray.
-const createNote = () => {
-
+// Write a function that creates a new Note Object, and adds the new Note Object to the notesArray.  Note should have an ID and text.  Then add the new note to the notesArray.  Then set the notesArray value to the newNotes array.
+const createNote = (text) => {
+  const newNote = {
+    id: nanoid(),
+    text: noteText,
+  }
+  const newNotes = [...notesArray, newNote]
+  setNotesArray(newNotes);
 }
 
 // Write a function that sets the value of the textarea as the noteText, 
 // if the event.target.value.length >= 0.
+const handleTextChange = (event) => {
+    setNoteText(event.target.value)
+    console.log(event.target.value);
+    createNote(noteText);
+}
+
 
 // Write a function that calls the createNote() function, with noteText passed in as a parameter.
 // Then set the value of the noteText from the textarea to be BLANK, so that a new note can be created. 
+const saveNote = () => {
+  if(noteText.trim() > 0) {
+    setNoteText('');
+  }
+}
+
 
 
   return (
@@ -29,12 +46,19 @@ const createNote = () => {
               rows="8" cols="10" 
               placeholder="Enter note here..." 
               name="note" 
-              id="note-input" />
+              id="note-input" >
+              </textarea>
             <small>chars remaining</small>
-            <button onClick={createNote} className="save btn">Save</button>
+            <button onClick={saveNote} className="save btn">Save</button>
         </div>  
         <section className="notes-list">
-          {/* map over the notesArray, and create a new note DIV for each one */}
+          <ul>
+            {notesArray.map((note) => {
+              return  <div key={nanoid()}>
+                {note.text}
+              </div>
+            })}
+          </ul>
         </section>  
     </div>
   );
